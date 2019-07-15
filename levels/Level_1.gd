a34bbf296a1352bd90d7b260212cd84d223f436a
manpacman <52835064+manpacman@users.noreply.github.com>
2019-07-14 23:44:04 -0300

    Update Level_1.gd

--- levels/Level_1.gd
+++ levels/Level_1.gd
@@ -6,13 +6,28 @@ func _ready():
 	$player.connect("dimension_swap", self, "_on_dimension_swap")
 
 func _on_dimension_swap():
-	if $solids.visible:
-		$solids.hide()
+	#if $solids.visible:
+	if $solids.modulate.a == 1:
+		#$solids.hide()
+		#$AlterDimension.show()
+		
+		$solids.modulate.a = 0.25
+		$AlterDimension.modulate.a = 1
+		
+		$solids.z_index = 100
+		$AlterDimension.z_index = -100
+		
 		$solids.set_collision_layer_bit(1,0)
-		$AlterDimension.show()
 		$AlterDimension.set_collision_layer_bit(1,1)
 	else:
-		$AlterDimension.hide()
+		#$AlterDimension.hide()
+		#$solids.show()
+		
+		$solids.modulate.a = 1
+		$AlterDimension.modulate.a = 0.25
+		
+		$solids.z_index = -100
+		$AlterDimension.z_index = 100
+		
 		$AlterDimension.set_collision_layer_bit(1,0)
-		$solids.show()
 		$solids.set_collision_layer_bit(1,1)
