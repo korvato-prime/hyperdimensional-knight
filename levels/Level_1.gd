0e9f4f580946b803ba3059a09fbf4e86e53066da
manpacman <52835064+manpacman@users.noreply.github.com>
2019-07-15 23:26:09 -0300

    Add files via upload

--- levels/Level_1.gd
+++ levels/Level_1.gd
@@ -4,9 +4,6 @@ var score = 0
 var highscore = Globals.highscore
 
 func _ready():
-	#$AlterDimension.hide()
-	$AlterDimension.show()
-	$solids.modulate.a = 0.15
 	_on_dimension_swap()
 	
 	$AlterDimension.set_collision_layer_bit(1,0)
@@ -14,33 +11,23 @@ func _ready():
 
 func _on_dimension_swap():
 	#if $solids.visible:
-	if $solids.modulate.a == 1:
-		#$solids.hide()
-		#$AlterDimension.show()
-		
+	if Globals.in_alter_dimension == true:
 		$solids.modulate.a = 0.25
-		$AlterDimension.modulate.a = 1
-		
+		$AlterDimension.modulate.a = 0.8
 		$solids.z_index = 100
 		$AlterDimension.z_index = -100
-		
 		$solids.set_collision_layer_bit(1,0)
 		$AlterDimension.set_collision_layer_bit(1,1)
-		
+		$permanent.position = Vector2()
 		Globals.in_alter_dimension = false
 	else:
-		#$AlterDimension.hide()
-		#$solids.show()
-		
-		$solids.modulate.a = 1
+		$solids.modulate.a = 0.8
 		$AlterDimension.modulate.a = 0.25
-		
 		$solids.z_index = -100
 		$AlterDimension.z_index = 100
-		
 		$AlterDimension.set_collision_layer_bit(1,0)
 		$solids.set_collision_layer_bit(1,1)
-		
+		$permanent.position = Vector2(0,-1216)
 		Globals.in_alter_dimension = true
 	
 	for points in get_tree().get_nodes_in_group("points"):
