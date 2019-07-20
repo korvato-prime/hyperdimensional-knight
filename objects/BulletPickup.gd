cee53da3dade05f4c6ce063e11df462dfdf26bce
manpacman <52835064+manpacman@users.noreply.github.com>
2019-07-20 16:55:07 -0300

    Add files via upload

--- objects/BulletPickup.gd
+++ objects/BulletPickup.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
 func _ready():
+	if Globals.in_alter_dimension == false:
+		set_modulate(Color(0.1,0.1,0.1))
 	$anim.play("anim")
 
 func _on_BulletPickup_body_entered(body):
@@ -9,7 +11,8 @@ func _on_BulletPickup_body_entered(body):
 
 func touched():
 	var player = get_tree().get_nodes_in_group("player")[0]
-	if player.ammo < player.ammo_max:
-		player.ammo += 1
+	player.ammo += 3
+	if player.ammo > player.ammo_max:
+		player.ammo = player.ammo_max
 	Globals.screen_shake(0.2, 15, 16)
 	queue_free()
