e9080c41ec475d98b3231c34a5c16cde938b0806
manpacman <52835064+manpacman@users.noreply.github.com>
2019-07-20 16:49:04 -0300

    Add files via upload

--- levels/Level_1.gd
+++ levels/Level_1.gd
@@ -5,8 +5,8 @@ var highscore = Globals.highscore
 var can_upgrade = 0
 var enemy_health_multiplier = 1
 var points_multiplier = 1
-
-signal change_form
+var min_enemies = 2
+var min_enemies_modifier = 10
 
 func _ready():
 	randomize()
@@ -15,15 +15,19 @@ func _ready():
 	
 	$AlterDimension.set_collision_layer_bit(1,0)
 	$player.connect("dimension_swap", self, "_on_dimension_swap")
+	
+	$Spawner_gunner/Timer.stop()
+	
 
 func _process(delta):
-	if $EnemyContainer.get_child_count() < 3:
+	if $EnemyContainer.get_child_count() < min_enemies:
 		randomize()
 		var to_generate = (randi() % 100) > 80
 		if to_generate:
-			var spawners = [$Spawner, $Spawner2, $Spawner3, $Spawner4]
+			var spawners = [$Spawner_walk, $Spawner_jumper, $Spawner_jumper2]
 			var spawner = spawners[randi() % spawners.size()]
-			spawner.spawn()
+			if not spawner.get_node("effect").visible:
+				spawner._on_Timer_timeout()
 
 func _on_dimension_swap():
 	#if $solids.visible:
@@ -56,7 +60,6 @@ func _on_dimension_swap():
 		$DesertLayer/PL04.visible = false
 		$DesertLayer/PL05.visible = false
 		$DesertLayer/PL06.visible = false
-		emit_signal("change_form")
 
 	else:
 		$solids.modulate.a = 0.8
@@ -92,7 +95,6 @@ func _on_dimension_swap():
 		$DesertLayer/PL05.visible = true
 		$DesertLayer/PL06.visible = true
 	
-	
 func actualize_score(points):
 	score += points
 	get_node("UI/Score").text = str(score)
@@ -100,11 +102,25 @@ func actualize_score(points):
 	for i in [10, 25, 50, 100, 200, 400, 600, 800, 1000]:
 		if i == score:
 			can_upgrade += 1
-			enemy_health_multiplier = enemy_health_multiplier + 1
+			#enemy_health_multiplier = enemy_health_multiplier + 1
 			points_multiplier = points_multiplier + 1
+	
+	if min_enemies_modifier == score:
+		min_enemies_modifier *= 2
+		min_enemies += 1
 	
 	if can_upgrade > 0:
 		$upgrade_area.visible = true
+	
+	if score == 50:
+		$Spawner_gunner.start()
+	
+	var spawners = [$Spawner_walk, $Spawner_jumper, $Spawner_jumper2]
+	var spawner = spawners[randi() % spawners.size()]
+	if spawner.minWaitTime > 10 and (randi() % 5 == 0):
+		spawner.minWaitTime -= 0.5
+	if spawner.maxWaitTime > 20 and (randi() % 5 == 0):
+		spawner.minWaitTime -= 0.5
 
 func game_over():
 	get_node("UI").volume = -20
